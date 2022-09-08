bc6672f1e5e05644c55b612b8b86c78bfb722221
Yongran <maiyongran@gmail.com>
2022-09-08 18:57:25 +0100

    add getAllPassengers

--- airline/src/main/java/com/example/airline/controllers/PassengerController.java
+++ airline/src/main/java/com/example/airline/controllers/PassengerController.java
@@ -1,4 +1,26 @@
 package com.example.airline.controllers;
 
+import com.example.airline.models.Passenger;
+import com.example.airline.services.PassengerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/passengers")
 public class PassengerController {
+
+    @Autowired
+    PassengerService passengerService;
+
+    @GetMapping
+    public ResponseEntity<List<Passenger>> getAllPassengers(){
+        List<Passenger> passengers = passengerService.getAllPassengers();
+        return new ResponseEntity<>(passengers, HttpStatus.OK);
+    }
 }
--- airline/src/main/java/com/example/airline/services/PassengerService.java
+++ airline/src/main/java/com/example/airline/services/PassengerService.java
@@ -1,6 +1,20 @@
 package com.example.airline.services;
 
+import com.example.airline.models.Passenger;
+import com.example.airline.repositories.PassengerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class PassengerService {
 
+    @Autowired
+    PassengerRepository passengerRepository;
+
+    public List<Passenger> getAllPassengers(){
+        return passengerRepository.findAll();
+    }
 
 }
