26a1901966ac32ec5c5c2c4c4a1a6283569d020d
Yongran <maiyongran@gmail.com>
2022-09-08 16:38:34 +0100

    create and annotate Flight

--- airline/src/main/java/com/example/airline/models/Flight.java
+++ airline/src/main/java/com/example/airline/models/Flight.java
@@ -1,4 +1,73 @@
 package com.example.airline.models;
 
+import javax.persistence.*;
+
+@Entity(name = "flights")
 public class Flight {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String destination;
+
+    @Column
+    private int capacity;
+
+    @Column(name = "departure_date")
+    private String departureDate;
+
+    @Column(name = "departure_time")
+    private String departureTime;
+
+    public Flight(String destination, int capacity, String departureDate, String departureTime) {
+        this.destination = destination;
+        this.capacity = capacity;
+        this.departureDate = departureDate;
+        this.departureTime = departureTime;
+    }
+
+    public Flight() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public String getDepartureDate() {
+        return departureDate;
+    }
+
+    public void setDepartureDate(String departureDate) {
+        this.departureDate = departureDate;
+    }
+
+    public String getDepartureTime() {
+        return departureTime;
+    }
+
+    public void setDepartureTime(String departureTime) {
+        this.departureTime = departureTime;
+    }
 }
