68ff0ba7d275d2e5f452f6ead455f7e0624b8d7b
Yongran <maiyongran@gmail.com>
2022-09-08 17:27:22 +0100

    add method to display flights

--- airline/src/main/java/com/example/airline/services/FlightService.java
+++ airline/src/main/java/com/example/airline/services/FlightService.java
@@ -3,9 +3,11 @@ package com.example.airline.services;
 import com.example.airline.models.Flight;
 import com.example.airline.repositories.FlightRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class FlightService {
 
     @Autowired
@@ -14,5 +16,9 @@ public class FlightService {
     public Flight addNewFlight(Flight flight){
         flightRepository.save(flight);
         return flight;
+    }
+
+    public List<Flight> getAllFlights() {
+        return flightRepository.findAll();
     }
 }
