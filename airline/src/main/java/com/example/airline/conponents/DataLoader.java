d00fb000c9b7bc57e1988f8b24d1673febd9a671
Yongran <maiyongran@gmail.com>
2022-09-08 19:14:32 +0100

    add component

--- airline/src/main/java/com/example/airline/conponents/DataLoader.java
+++ airline/src/main/java/com/example/airline/conponents/DataLoader.java
@@ -7,7 +7,9 @@ import com.example.airline.repositories.PassengerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
 
+@Component
 public class DataLoader implements ApplicationRunner {
 
     @Autowired
