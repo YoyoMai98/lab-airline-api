1684fd465f8183e5c7029c76de5a382a78abc771
Yongran <maiyongran@gmail.com>
2022-09-08 16:35:48 +0100

    create and annotate Passenger

---
+++ airline/src/main/java/com/example/airline/models/Flight.java
@@ -0,0 +1,4 @@
+package com.example.airline.models;
+
+public class Flight {
+}
---
+++ airline/src/main/java/com/example/airline/models/Passenger.java
@@ -0,0 +1,49 @@
+package com.example.airline.models;
+
+import javax.persistence.*;
+
+@Entity(name = "passengers")
+public class Passenger {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    public Passenger(String name, String phoneNumber) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Passenger() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
